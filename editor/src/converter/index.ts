487f3dc9661e1caab252946c19a6ca4f34adfb36
Ryan Mitchell <rionjosef@gmail.com>
2026-04-07 15:59:41 -0700

    feat: add converter barrel export
    Re-exports convertDocument, convertBlock, convertInline, and all
    converter output types from a single entry point.

---
+++ editor/src/converter/index.ts
@@ -0,0 +1,12 @@
+export { convertDocument } from "./converter";
+export { convertBlock } from "./block-converter";
+export { convertInline } from "./inline-converter";
+export type {
+  BNBlock,
+  BNInlineContent,
+  BNStyledText,
+  BNLink,
+  BNTableContent,
+  BNTableRow,
+  ConvertOptions,
+} from "./types";
