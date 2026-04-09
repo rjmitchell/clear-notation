4b647813eafa2eae6abbfb557a6b0941577ac994
Ryan Mitchell <rionjosef@gmail.com>
2026-04-08 20:38:15 -0700

    chore: move editor to /editor/ subpath

--- editor/vite.config.ts
+++ editor/vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/clear-notation/",
+  base: "/clear-notation/editor/",
   root: ".",
   build: { outDir: "dist" },
   plugins: [react()],
